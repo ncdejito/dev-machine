eaf45d3c948d04e8fdf30cd91bbe8b46490a4e7d
Niccolo Dejito <ncdejito@gmail.com>
2022-10-30 05:57:44 +0800

    Add auto yes flag

--- apps/vscode.sh
+++ apps/vscode.sh
@@ -1,8 +1,8 @@
 
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install -y software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt install -y code
 
 code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
