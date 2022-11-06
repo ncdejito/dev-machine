015435f736ec9d9f7ccec1a5568f101048696068
Niccolo Dejito <ncdejito@gmail.com>
2022-11-07 04:47:30 +0800

    Add more apps

---
+++ apps/rpi-imager.sh
@@ -0,0 +1,3 @@
+sudo apt update
+wget https://downloads.raspberrypi.org/imager/imager_latest_amd64.deb
+sudo apt --fix-broken install imager_latest_amd64.deb
--- apps/vscode.sh
+++ apps/vscode.sh
@@ -6,3 +6,4 @@ sudo apt install -y code
 
 code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools-extension-pack
