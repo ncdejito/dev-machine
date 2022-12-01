c6a1ea6ac3953daa7dabdde685d681fc3401a34b
Niccolo Dejito <ncdejito@gmail.com>
2022-12-01 16:05:21 +0800

    Update

--- apps/discord.sh
+++ apps/discord.sh
@@ -1,1 +1,5 @@
-sudo snap install discord
+
+sudo apt update
+sudo apt install -y gdebi-core wget
+wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo gdebi ~/discord.deb 
