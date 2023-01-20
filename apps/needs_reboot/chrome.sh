2b9eb75c383ad22aa12148dcb114222cda917000
Niccolo Dejito <ncdejito@gmail.com>
2023-01-20 12:06:27 +0800

    Add config comments to chrome

--- apps/needs_reboot/chrome.sh
+++ apps/needs_reboot/chrome.sh
@@ -1,3 +1,9 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo reboot
+
+# ## Config
+# [Custom search](https://www.howtogeek.com/714040/how-to-fix-and-use-custom-searches-keywords-in-google-chrome/)
+
+# Reopen session when turning off computer
+# Settings > On startup > Continue where you left off
