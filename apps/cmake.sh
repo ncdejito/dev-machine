482709d3e3b5d21f7d984e454012bf353b391bda
Niccolo Dejito <ncdejito@gmail.com>
2023-01-31 14:48:46 +0800

    Add cmake

---
+++ apps/cmake.sh
@@ -0,0 +1,13 @@
+sudo apt install cmake
+
+# wget https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2.tar.gz
+# tar -zxvf cmake-3.25.2.tar.gz
+# ./bootstrap
+# make
+# make install
+
+
+
+# CMake Error at Utilities/cmcurl/CMakeLists.txt:608 (message):
+#   Could not find OpenSSL.  Install an OpenSSL development package or
+#   configure CMake with -DCMAKE_USE_OPENSSL=OFF to build without OpenSSL.
