dfa8a4b14f94c044e8f7f29cd7a3f2a2ef836b69
Niccolo Dejito <ncdejito@gmail.com>
2025-05-06 13:22:48 +0800

    Update chrome.sh

--- apps/needs_reboot/chrome.sh
+++ apps/needs_reboot/chrome.sh
@@ -4,6 +4,7 @@ sudo reboot
 
 # ## Config
 # [Custom search](https://www.howtogeek.com/714040/how-to-fix-and-use-custom-searches-keywords-in-google-chrome/)
+# Prevent chrome asking password every open: https://askubuntu.com/a/968149
 
 # Reopen session when turning off computer
 # Settings > On startup > Continue where you left off
